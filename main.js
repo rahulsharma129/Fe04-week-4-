fec2c84d71ffc78405ff0cb1bc704a46109f88a3
Rahul Sharma <rahulakash123456@gmail.com>
2026-01-13 20:24:41 +0530

    basic

--- main.js
+++ main.js
@@ -9,29 +9,81 @@ const filters = {
         value: 100,
         min : 0,
         max : 200,
+        unit : "%"
     },
     exposure: {
         value: 100,
         min : 0,
-        max : 200
+        max : 200,
+        unit : "%"
     },
     saturation: {
         value: 100,
         min : 0,
-        max : 200
+        max : 200,
+        unit : "%"
     },
     hueRotation: {
         value: 0,
         min : 0,
-        max : 360
+        max : 360,
+        unit : "deg",
     },
     blur:{
         value: 0,
         min : 0,
-        max : 20
+        max : 20,
+        unit : "px",
     },
-    grayscale:0,
-    sepia:0,
-    opacity:100,
-    invert:0,
+    grayscale:{
+        value: 0,
+        min : 0,
+        max : 100,
+        unit : "%",
+    },
+    sepia:{
+        value: 0,
+        min : 0,
+        max : 100,
+        unit : "%",
+    },
+    opacity:{
+        value: 100,
+        min : 0,
+        max : 100,
+        unit : "%",
+    },
+    invert:{
+        value: 0,
+        min : 0,
+        max : 100,
+        unit : "%", 
+    },
 }
+
+const imageCanvas = document.querySelector("#image-canvas");
+const filtersContainer = document.querySelector(".filters");
+
+function createFilterElement(name, unit="%", value, min, max){
+
+    const div = document.createElement("div");
+    div.classList.add("filter");
+
+    const input = document.createElement("input");
+    input.type = "range";
+    input.min = min;
+    input.max = max;
+    input.value = value;
+    input.id = name;
+
+    const p = document.createElement("p");
+    p.innerText = name;
+
+    div.appendChild(p);
+    div.appendChild(input);
+}
+
+Object.keys(filters).forEach(key => {
+    const filterElement = createFilterElement(key, filters[key].unit, filters[key].value, filters[key].min, filters[key].max);
+    filtersContainer.appendChild(filterElement);
+})
