24785343c8a2b9e96cdc935f85c36ee5fab249bc
Rahul Sharma <rahulakash123456@gmail.com>
2026-01-13 15:31:42 +0530

    HC Completed

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-.body{
-    margin: 0;
-    padding: 0;
+const filters = {
+    brightness: {
+        value: 100,
+        min : 0,
+        max : 200,
+        unit: "%"
+    },
+    contrast: {
+        value: 100,
+        min : 0,
+        max : 200,
+    },
+    exposure: {
+        value: 100,
+        min : 0,
+        max : 200
+    },
+    saturation: {
+        value: 100,
+        min : 0,
+        max : 200
+    },
+    hueRotation: {
+        value: 0,
+        min : 0,
+        max : 360
+    },
+    blur:{
+        value: 0,
+        min : 0,
+        max : 20
+    },
+    grayscale:0,
+    sepia:0,
+    opacity:100,
+    invert:0,
 }
